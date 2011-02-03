e22e0834d75726c0f0bcc8d51738b3b89b180296
Ethan Chen <intervigil@gmail.com>
2011-02-02 23:17:52 -0800

    update copyright notice dates

--- src/com/intervigil/wave/WaveWriter.java
+++ src/com/intervigil/wave/WaveWriter.java
@@ -1,6 +1,6 @@
 /* WaveWriter.java
 
-   Copyright (c) 2010 Ethan Chen
+   Copyright (c) 2011 Ethan Chen
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
